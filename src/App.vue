50ad3f55cd0e733d952d0922d1f06de634ff7929
lamas250 <igorlamas1994@gmail.com.br>
2020-11-06 23:32:34 -0300

    conversor com api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL"></Conversor>
+      <Conversor moeda-a="BRL" moeda-b="USD"></Conversor>
+    </div>
+      <div class="linha">
+      <Conversor moeda-a="CAD" moeda-b="BRL"></Conversor>
+      <Conversor moeda-a="BRL" moeda-b="CAD"></Conversor>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversor from "./components/Conversor";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor,
   }
 }
 </script>
@@ -24,5 +31,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 800px;
+}
+
+.linha{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
 }
 </style>
